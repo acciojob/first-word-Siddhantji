c349b182c2d5eebe6efd8185e006966f6fcf1874
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-08 13:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	if (!str || !str.includes(' ')) {
+        return str;
+    }
+    // Split the string by spaces and return the first element
+    return str.split(' ')[0];
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
